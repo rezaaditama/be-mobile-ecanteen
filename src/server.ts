80f84cf3eca0eebb13e4c7c851af10b570c21aee
Reza Aditama <aditamareza03@gmail.com>
2026-01-17 09:50:12 +0700

    hosting to vercel

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,13 @@ import app, { logger } from './app';
 // Port berasal dari env
 const PORT = Number(env.PORT);
 
-// Jalankan ketika server dimulai
-app.listen(PORT, () => {
-  logger.info(`Server running in ${env.NODE_ENV} http://localhost:${PORT}`);
-});
+// Jalankan ketika di local seperti ini
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = Number(env.PORT) || 3000;
+  app.listen(PORT, () => {
+    logger.info(`Server running in ${env.NODE_ENV} http://localhost:${PORT}`);
+  });
+}
+
+// WAJIB: Export app agar Vercel bisa menjadikannya serverless function
+export default app;
